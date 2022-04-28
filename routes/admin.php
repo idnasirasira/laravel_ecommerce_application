5780b5e18851aac6fc492465ef3f722ec05d3b61
Aris Arisandi <idnasirasira@gmail.com>
2022-04-28 14:21:21 +0700

    Dashboard Admin route

--- routes/admin.php
+++ routes/admin.php
@@ -8,3 +8,11 @@ Route::name('admin.')->prefix('admin')->group(function(){
     Route::post('/login', [LoginController::class, 'login'])->name('login.post');
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
+
+Route::group(['middleware' => ['auth:admin']], function () {
+
+    Route::get('/', function () {
+        return view('admin.dashboard.index');
+    })->name('admin.dashboard');
+
+});
